878d481a6337e020c57c2d2f7383243b43778853
Landon Ellis <landon.ellis2@gmail.com>
2020-06-25 00:26:48 -0400

    Initial commit

---
+++ src/com/coot/SpigCoot.java
@@ -0,0 +1,21 @@
+package com.coot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpigCoot extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+		this.getLogger().warning("\n=============TEST=============\n");
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+		
+		
+	}
+	
+}
