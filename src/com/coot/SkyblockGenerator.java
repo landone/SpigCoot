8c90d5de750d5b61f3a7b8c8165a8ca290289fbb
Landon Ellis <landon.ellis2@gmail.com>
2020-06-25 23:36:08 -0400

    Skybock

---
+++ src/com/coot/SkyblockGenerator.java
@@ -0,0 +1,85 @@
+package com.coot;
+
+import java.util.Random;
+
+import org.bukkit.Chunk;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
+import org.bukkit.block.Chest;
+import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class SkyblockGenerator extends ChunkGenerator {
+	
+	@Override
+	public ChunkData generateChunkData(World world, Random rand, int chunkX, int chunkZ, BiomeGrid biome) {
+		
+		ChunkData data = createChunkData(world);
+		
+		if (chunkX == 0 && chunkZ == 0) {
+			int height = 100;
+			
+			for (int x = 0; x < 16; x++) {
+				for (int z = 0; z < 16; z++) {
+					
+					data.setBlock(x, height, z, Material.GRASS_BLOCK);
+					data.setBlock(x, height - 1, z, Material.DIRT);
+					
+				}
+			}
+			
+			data.setBlock(8, height + 1, 8, Material.CHEST);
+			
+		}
+		
+		return data;
+		
+	}
+	
+	public void regenerateChunk(World world, int chunkX, int chunkZ) {
+		
+		ChunkData data = generateChunkData(world, null, chunkX, chunkZ, null);
+		Chunk chunk = world.getChunkAt(chunkX, chunkZ);
+		for (int y = 0; y < 256; y++) {
+			for (int x = 0; x < 16; x++) {
+				for (int z = 0; z < 16; z++) {
+					
+					chunk.getBlock(x, y, z).setType(data.getType(x, y, z));
+					
+				}
+			}
+		}
+		
+		if (chunkX == 0 && chunkZ == 0) {
+			
+			Chest chest = (Chest) world.getBlockAt(8, 101, 8).getState();
+			Inventory inven = chest.getInventory();
+			inven.clear();
+			ItemStack[] items = new ItemStack[4];
+			items[0] = new ItemStack(Material.DIRT, 26);
+			items[1] = new ItemStack(Material.OAK_SAPLING, 1);
+			items[2] = new ItemStack(Material.LAVA_BUCKET, 1);
+			items[3] = new ItemStack(Material.ICE, 1);
+			inven.addItem(items);
+			
+		}
+		
+	}
+	
+	public World createWorld() {
+		
+		WorldCreator wcr = new WorldCreator("skyblock");
+		wcr.generator(this);
+		World world = wcr.createWorld();
+		regenerateChunk(world, 0, 0);
+		world.setAnimalSpawnLimit(0);
+		world.setMonsterSpawnLimit(0);
+		world.setSpawnLocation(7, 101, 7);
+		
+		return world;
+		
+	}
+
+}
--- src/com/coot/SpigCoot.java
+++ src/com/coot/SpigCoot.java
@@ -1,20 +1,50 @@
 package com.coot;
 
+import java.util.logging.Logger;
+
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SpigCoot extends JavaPlugin {
 	
+	Logger log = this.getLogger();
+	World skyblock;
+	SkyblockGenerator skyGen = new SkyblockGenerator();
+	
 	@Override
 	public void onEnable() {
 		
-		this.getLogger().warning("\n=============TEST=============\n");
-		
+		skyblock = skyGen.createWorld();
+	
 	}
 	
 	@Override
 	public void onDisable() {
 		
 		
+		
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		
+		if (!(sender instanceof Player)) {
+			return false;
+		}
+		
+		Player player = (Player)sender;
+		
+		switch(label) {
+		case "sky":
+			player.teleport(skyblock.getSpawnLocation(), TeleportCause.COMMAND);
+			break;
+		}
+		
+		return true;
 		
 	}
 	
